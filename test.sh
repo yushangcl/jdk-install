3b0b3362a6be858bef4490d68b5f91c5ec13d77a
log.r <log.r@live.cn>
2018-05-04 22:21:59 +0800

    init

--- test.sh
+++ test.sh
@@ -24,12 +24,10 @@ if [[ "$JAVA_VERSION" == java* ]]; then
 fi
 #Prepare the installation environment
 echo -e ""
-echo -e "Prepare the installation environment."
 if cat /etc/*-release | grep -Eqi "centos|red hat|redhat"; then
   echo "RPM-based"
   yum -y install wget
 elif cat /etc/*-release | grep -Eqi "debian|ubuntu|deepin"; then
-  echo "Debian-based"
   apt-get -y install wget
 else
   echo "This release is not supported."
@@ -52,21 +50,17 @@ fi
 
 # 用户选择安装的jdk版本 默认jdk8
 echo "请选择需要安装的jdk版本(默认安装jdk8)"
-echo "jdk7 请输入：7"
-echo "jdk8 请输入：8"
-echo "jdk10 请输入：10"
+echo "1. jdk7 请输入：7"
+echo "2. jdk8 请输入：8"
+echo "3. jdk10 请输入：10"
 read -p "请输入：" val
 if [  ! -n "$val" ]; then
-    echo "*******开始下载默认jdk8*******"
-    if [ "$arch" -eq 32 ]; then
-      jdk=jdk-8u171-linux-i586.tar.gz
-      wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie"  -P /var/tmp/jdk "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/$jdk"
-    else
-      jdk=jdk-8u171-linux-x64.tar.gz
-      wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" -P /var/tmp/jdk "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/$jdk"
-    fi
-elif [ "$val" -eq 7 ]; then
-    echo "*******开始下载jdk7*******"
+    echo "设置默认版本jdk8"
+    val=8
+fi
+
+case $val in
+   7) echo "*******开始下载jdk7*******"
     if [ "$arch" -eq 32 ]; then
       jdk=jdk-7u80-linux-i586.tar.gz
       # 由于oracle官网需要登录下载jdk7 则现在从cos下载
@@ -74,29 +68,26 @@ elif [ "$val" -eq 7 ]; then
     else
       jdk=jdk-7u80-linux-x64.tar.gz
       wget http://dev-1251506639.cossh.myqcloud.com/jdk/jdk7/"$jdk" -P /var/tmp/jdk
-    fi
-elif [ "$val" -eq 8 ];then
-    echo "*******开始下载jdk8*******"
+    fi;;
+    8) echo "*******开始下载jdk8*******"
      if [ "$arch" -eq 32 ]; then
       jdk=jdk-8u171-linux-i586.tar.gz
       wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie"  -P /var/tmp/jdk "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/$jdk"
     else
       jdk=jdk-8u171-linux-x64.tar.gz
       wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" -P /var/tmp/jdk "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/$jdk"
-    fi
-elif [ "$val" -eq 10 ];then
-    echo "*******开始下载jdk10*******"
+    fi;;
+    10) echo "*******开始下载jdk10*******"
     if [ "$arch" -eq 32 ]; then
       echo "*******jdk10不支持32位操作系统，请选择64位安装*******"
     else
       jdk=jdk-10.0.1_linux-x64_bin.tar.gz
       wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" -P /var/tmp/jdk "http://download.oracle.com/otn-pub/java/jdk/10.0.1+10/fb4372174a714e6b8c52526dc134031e/$jdk"
-    fi
-else
-    echo "请输入有效的数字"
+    fi;;
+    *) echo "请输入有效的数字"
     echo ""
-    exit
-fi
+    exit 1;;
+esac
 
 #tar mv /usr/local/jdk
 echo "*******开始解压*******"
