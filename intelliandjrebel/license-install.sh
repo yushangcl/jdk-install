3e4ca812db18974e836f0bf4abf8a60aa5534ed9
huahui.wu <huahui.wu@wwwarehouse.com>
2018-05-09 10:50:49 +0800

    init

--- intelliandjrebel/license-install.sh
+++ intelliandjrebel/license-install.sh
@@ -10,7 +10,8 @@ echo "    #                Version 0.4.1                 #"
 echo "    ################################################"
 echo
 
-license_install_path="usr/local/app/JetLicense/"
+license_install_path="/usr/local/JetLicense"
+license_conf="/usr/local/JetLicense/license.conf"
 
 # 设置展示颜色
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
@@ -40,7 +41,7 @@ check_sys(){
 }
 
 #检查安装状态
-check_installed_status(){
+check_java_installed_status(){
    java=`java -version 2>&1 | head -1`
    if [[ "$java" == java* ]]; then
      echo ""
@@ -54,40 +55,95 @@ check_pid(){
     PID=$(ps -ef | grep LicenseServer-1.0-jar-with-dependencies.jar | grep -v grep | awk '{print $2}')
 }
 
+#设置端口
+Set_port(){
+    while true
+        do
+        echo -e "请输入 LicenseServer 监听端口 [1-65535]"
+        stty erase '^H' && read -p "(默认端口: 8081):" ct_port
+        [[ -z "${ct_port}" ]] && ct_port="8081"
+        expr ${ct_port} + 0 &>/dev/null
+        if [[ $? -eq 0 ]]; then
+            if [[ ${ct_port} -ge 1 ]] && [[ ${ct_port} -le 65535 ]]; then
+                echo && echo "========================"
+                echo -e "   端口 : ${Red_background_prefix} ${ct_port} ${Font_color_suffix}"
+                echo "========================" && echo
+                break
+            else
+                echo "输入错误, 请输入正确的端口。"
+            fi
+        else
+            echo "输入错误, 请输入正确的端口。"
+        fi
+    done
+}
+
+#写入配置文件
+Write_config(){
+mkdir ${license_install_path}
+cat > ${license_conf}<<-EOF
+    port = ${port}
+EOF
+}
+
+#读取配置文件
+Read_config(){
+    [[ ! -e ${license_conf} ]] && echo -e "${Error} JetLicense 配置文件不存在 !" && exit 1
+    port=`cat ${license_conf}|grep "port = "|awk -F "port = " '{print $NF}'`
+}
+
 #安装
 Install_license(){
-    wget -P ${license_install_path} "https://github.com/yushangcl/LicenseServer/releases/download/1.0.0/LicenseServer-1.0-jar-with-dependencies.jar"
+    Set_port
+    Write_config
+    wget -P ${license_install_path/} "https://github.com/yushangcl/LicenseServer/releases/download/1.0.0/LicenseServer-1.0-jar-with-dependencies.jar"
 }
 
 #卸载
 Uninstall_license(){
     rm -rf ${license_install_path}
+
+    echo "确定要卸载 LicenseServer ? (y/N)"
+	echo
+	stty erase '^H' && read -p "(默认: n):" unyn
+	[[ -z ${unyn} ]] && unyn="n"
+	if [[ ${unyn} == [Yy] ]]; then
+		check_pid
+		[[ ! -z $PID ]] && kill -9 ${PID}
+		rm -rf ${license_install_path}
+		echo && echo "LicenseServer 卸载完成 !" && echo
+	else
+		echo && echo "卸载已取消..." && echo
+	fi
 }
 
 #启动
 Start_license(){
-    check_installed_status
+    check_java_installed_status
     check_pid
+    Read_config
     [[ ! -z ${PID} ]] && echo -e "${Error} JetLicense 正在运行，请检查 !" && exit 1
-    nohup java -jar ${license_install_path}/LicenseServer-1.0-jar-with-dependencies.jar  -p 8081 >license.out 2>&1 &
-    exit 1
+    nohup java -jar ${license_install_path}/LicenseServer-1.0-jar-with-dependencies.jar  -p ${port} >${license_install_path}/license.out 2>&1 &
+    echo -e "${Info} JetLicense 启动成功！ \n${Info} 日志文件：${license_install_path}/license.out"
 }
 
 #停止
 Stop_license(){
-    check_installed_status
+    check_java_installed_status
     check_pid
     [[ -z ${PID} ]] && echo -e "${Error} JetLicense 没有运行，请检查 !" && exit 1
     kill ${PID}
-    exit 1
+    echo -e "${Info} JetLicense 已停止！"
 }
 
 #重启
 Restart_license(){
-    check_installed_status
+    check_java_installed_status
     check_pid
+    Read_config
     [[ ! -z ${PID} ]] &&  kill ${PID}
-    nohup java -jar ${license_install_path}/LicenseServer-1.0-jar-with-dependencies.jar  -p 8081 >license.out 2>&1 &
+    nohup java -jar ${license_install_path}/LicenseServer-1.0-jar-with-dependencies.jar  -p ${port} >license.out 2>&1 &
+    echo -e "${Info} JetLicense 重启成功！ \n${Info} 日志文件：${license_install_path}/license.out"
 }
 
 echo && echo -e "请输入一个数字来选择选项
@@ -123,7 +179,6 @@ case "$num" in
 	echo "该功能未实现" exit 1
 	;;
 	3)
-	Stop_license
 	Uninstall_license
 	;;
 	4)
