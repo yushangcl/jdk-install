1a89df4308a8506274b6d09ea560b796e35cb1a2
log.r <log.r@live.cn>
2018-05-10 19:52:40 +0800

    init

---
+++ get-git.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/yushangcl/shell-install.git
+rm -rf /www/wwwroot/down.itbat.cn/shell/shell-install
+mv shell-install /www/wwwroot/down.itbat.cn/shell/
